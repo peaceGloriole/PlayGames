33b7093c22dae9e3df5c0d5713dd1bd052f5c5fe
peaceGloriole <dmtrpwlw@icloud.com>
2024-08-08 21:25:38 +0300

    create game page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/header/header/Header"
 import HomePage from "./components/header/homepage/HomePage"
 import Login from "./components/header/login/Login"
 import Register from "./components/header/register/Register"
+import CreateGame from "./components/create/CreateGame"
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/create" element={<CreateGame />} />
                 </Routes>
             </main>
 
