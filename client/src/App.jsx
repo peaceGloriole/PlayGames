9a4c06a384ddbf8da42082c3c02f46c54b8ba3b5
peaceGloriole <dmtrpwlw@icloud.com>
2024-08-08 21:18:18 +0300

    login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,23 @@ import { Routes, Route } from "react-router-dom"
 
 import Header from "./components/header/header/Header"
 import HomePage from "./components/header/homepage/HomePage"
+import Login from "./components/header/login/Login"
 
 function App() {
 
-  return (
-    <div id="box">
-      <Header />
+    return (
+        <div id="box">
+            <Header />
 
-      <main id="main-content">
-        <Routes>
-      <Route path="/" element={<HomePage />} />
-        </Routes>
-      </main>
+            <main id="main-content">
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </main>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default App
