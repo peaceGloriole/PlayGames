224d571c20023cd4dad0f076d7682d77f7b15e9e
peaceGloriole <dmtrpwlw@icloud.com>
2024-08-08 21:09:31 +0300

    cleaned

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom"
+
+import Header from "./components/header/Header"
+
+function App() {
+
+  return (
+    <div id="box">
+      <Header />
+
+      <main id="main-content">
+        <Routes>
+          
+        </Routes>
+      </main>
+
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                <a className="home" href="/">GamesPlay</a>
+            </h1>
+            <nav>
+                <a href="/games">All games</a>
+                <div id="user">
+                    <a href="/create">Create Game</a>
+                    <a href="/logout">Logout</a>
+                </div>
+                <div id="guest">
+                    <a href="/login">Login</a>
+                    <a href="/register">Register</a>
+                </div>
+            </nav>
+        </header>
+    );
+}
